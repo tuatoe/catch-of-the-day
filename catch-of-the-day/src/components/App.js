baf8d5695f8104abd37a019edb2e66ca2ffc8de6
Tuatoe <tuatoe.daynuah@gmail.com>
2018-03-11 18:41:01 -0500

    build initial components

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,8 +2,31 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 export default class App extends React.Component{
+    state = {
+        fishes: {},
+        order: {},
+    };
+
+    addFish = fish => {
+       //take a copy of the exsiting state
+       const fishes = { ...this.state.fishes };
+       //add our new fish to the fishes var
+       fishes[`fish${Date.now()}`] = fish;
+       //set the new fishes obj to state
+       this.setState({
+           fishes,
+       })
+    };
+
+    loadSampleFishes = () => {
+        this.setState({
+            fishes: sampleFishes,
+        })
+    };
+
     render(){
         return (
             <div className="catch-of-the-day"> 
@@ -11,7 +34,7 @@ export default class App extends React.Component{
                     <Header tagline="Fresh Seafood Market" />
                </div>
                <Order />
-               <Inventory />
+               <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
             </div>
         );
     }
--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import AddFishForm from './AddFishForm';
 
 export default class Inventory extends React.Component{
     render(){
         return (
            <div className="inventory">
-               Inventory
+               <h2>Inventory</h2>
+               <AddFishForm addFish={this.props.addFish} />
+               <button onClick={this.props.loadSampleFishes}>Load Sample Fishes</button>
            </div>
-        )
+        );
     }
 }
